11f359db86ef469394a6b2e67cbaf2e95c2b2be9
Mock <qingmingmock@outlook.com>
2025-06-17 15:02:07 +0800

    update merge.py

--- data/merge.py
+++ data/merge.py
@@ -6,12 +6,14 @@ test = pd.read_csv('data/test.csv')
 oil = pd.read_csv('data/oil.csv')
 transactions = pd.read_csv('data/transactions.csv')
 stores = pd.read_csv('data/stores.csv')
+holidays = pd.read_csv('data/holidays_events.csv')
 
 # ensure the type is datetime
 train['date'] = pd.to_datetime(train['date'])
 test['date'] = pd.to_datetime(test['date'])
 oil['date'] = pd.to_datetime(oil['date'])
 transactions['date'] = pd.to_datetime(transactions['date'])
+holidays['date'] = pd.to_datetime(holidays['date'])
 
 # merge oil.csv
 train = train.merge(oil, on='date', how='left')
@@ -28,8 +30,12 @@ test = test.merge(stores, on='store_nbr', how='left')
 
 # one-hot encoding?
 
+
+
 # save the merged files
 train.to_csv('data/output/train_merged.csv', index=False)
+print("train.csv has been merged.")
 test.to_csv('data/output/test_merged.csv', index=False)
+print("test.csv has been merged.")
 
-print("Has been merged.")
+print("Merger completed")
