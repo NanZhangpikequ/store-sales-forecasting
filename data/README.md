04f29fa84bc7bc27a0b0a7cf396b97264d37a8c5
Mock <qingmingmock@outlook.com>
2025-06-17 15:01:39 +0800

    add merge.py to merge csv, without holidays_events.csv

--- data/README.md
+++ data/README.md
@@ -1,1 +1,2 @@
-
+Please put the original .csv file directly in the /data folder
+Then run the merge.py, and the merged files will be saved in the output folder.
---
+++ data/merge.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+# read csv files
+train = pd.read_csv('data/train.csv')
+test = pd.read_csv('data/test.csv')
+oil = pd.read_csv('data/oil.csv')
+transactions = pd.read_csv('data/transactions.csv')
+stores = pd.read_csv('data/stores.csv')
+
+# ensure the type is datetime
+train['date'] = pd.to_datetime(train['date'])
+test['date'] = pd.to_datetime(test['date'])
+oil['date'] = pd.to_datetime(oil['date'])
+transactions['date'] = pd.to_datetime(transactions['date'])
+
+# merge oil.csv
+train = train.merge(oil, on='date', how='left')
+test = test.merge(oil, on='date', how='left')
+# merge transcations.csv
+train = train.merge(transactions, on=['date', 'store_nbr'], how='left')
+test = test.merge(transactions, on=['date', 'store_nbr'], how='left')
+# merge store.csv
+train = train.merge(stores, on='store_nbr', how='left')
+test = test.merge(stores, on='store_nbr', how='left')
+# merge holidays_events.csv
+
+
+
+# one-hot encoding?
+
+# save the merged files
+train.to_csv('data/output/train_merged.csv', index=False)
+test.to_csv('data/output/test_merged.csv', index=False)
+
+print("Has been merged.")
